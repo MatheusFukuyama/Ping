cefee03e8f1fea8b573c134a034fa342f611e7a6
MatheusFukuyama <mama.fukuyama@gmail.com>
2021-10-20 03:35:06 -0300

    projeto completo

--- js/script.js
+++ js/script.js
@@ -12,19 +12,18 @@ function verifyEmail(e) {
     e.preventDefault();
     
     const email = document.getElementsByTagName("input")[0];
-    const errorSpace = document.getElementById("error-space");
+    const errorSpace = document.getElementsByClassName("error-space")[0];
+    const cl3 = document.getElementsByClassName("cl-3")[0]
     const message = document.createElement("span")
 
-    message.style.fontSize = "11px"
-    message.style.color = "var(--secundary-red)"
-    message.style.fontStyle = "italic"
-
-    errorSpace.style.padding = "0px 20px"
+    errorSpace.classList.add("error-space")
+    message.classList.add("message")
   
     if(!ValidateEmail(email.value) && !appended) {
       email.classList.add("error")
       message.innerHTML = "Please provide a valid email address"
       errorSpace.appendChild(message)
+      cl3.classList.add("space-btn-error")
       appended = 1
     }
     else if(ValidateEmail(email.value)){
