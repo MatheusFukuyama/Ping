38a0d788c4480f18244aaff0e0599b742140f9f7
MatheusFukuyama <mama.fukuyama@gmail.com>
2021-10-21 15:29:55 -0300

    adicionado mensagem de email vazio

--- js/script.js
+++ js/script.js
@@ -7,6 +7,13 @@ function ValidateEmail(email)
     return (false)
 }
 
+function empatyEmail(email) {
+    if(email.trim() === "") {
+       return 0
+    }
+    return 1
+}
+
 appended = 0;
 function verifyEmail(e) {
     e.preventDefault();
@@ -14,19 +21,38 @@ function verifyEmail(e) {
     const email = document.getElementsByTagName("input")[0];
     const errorSpace = document.getElementsByClassName("error-space")[0];
     const cl3 = document.getElementsByClassName("cl-3")[0]
-    const message = document.createElement("span")
 
-    errorSpace.classList.add("error-space")
-    message.classList.add("message")
-  
-    if(!ValidateEmail(email.value) && !appended) {
+    let message
+    if(appended) {
+      message = document.getElementsByTagName("span")[1]
+      console.log(message)
+    }
+    else {
+      message = document.createElement("span")
+      errorSpace.classList.add("error-space")
+      message.classList.add("message")
+    }
+      
+
+    if(!empatyEmail(email.value)) {
       email.classList.add("error")
-      message.innerHTML = "Please provide a valid email address"
-      errorSpace.appendChild(message)
-      cl3.classList.add("space-btn-error")
+      message.innerHTML = "Whoops! It looks like you forgot to add your email"
+      if(!appended){
+        errorSpace.appendChild(message)
+        cl3.classList.add("space-btn-error")
+      }
       appended = 1
     }
-    else if(ValidateEmail(email.value)){
+    else if(!ValidateEmail(email.value)) {
+      email.classList.add("error")
+      message.innerHTML = "Please provide a valid email address"
+      if(!appended){
+        errorSpace.appendChild(message)
+        cl3.classList.add("space-btn-error")
+      }
+        appended = 1
+    }
+    else{
         email.classList.remove("error")
         errorSpace.innerHTML = ""
         appended = 0
